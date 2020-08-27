91d6236a0e1fd316a646b129cbbc5b981e46f088
perinazzoo <gabrielperinazo@gmail.com>
2020-08-27 19:38:07 -0300

    remove bg

--- gobarber-web/src/pages/_layouts/auth/index.js
+++ gobarber-web/src/pages/_layouts/auth/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Moon from '~/components/Bg-moon';
 
 import { Wrapper, Content } from './styles';
 
 export default function AuthLayout({ children }) {
   return (
     <Wrapper>
-      <Moon />
       <Content>{children}</Content>
     </Wrapper>
   );
--- gobarber-web/src/pages/_layouts/default/index.js
+++ gobarber-web/src/pages/_layouts/default/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '~/components/Header';
-import Moon from '~/components/Bg-moon';
 
 import { Wrapper, Content } from './styles';
 
 export default function DefaultLayout({ children }) {
   return (
     <Wrapper>
-      <Moon />
       <Header />
       <Content>{children}</Content>
     </Wrapper>
